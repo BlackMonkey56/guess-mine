e54ef824254ff68c9c25dbd50fb7deadaf312b8f
BlackMonkey56 <blackmonkey9256@gmail.com>
2020-05-10 00:26:32 +0900

    Set up socket.io

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 import express from "express";
-import path from "path";
+import { join } from "path";
 import socketIO from "socket.io";
+import logger from "morgan";
 
 const PORT = 4000;
 const app = express();
-app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
-app.set(express.static(path.join(__dirname, "static")));
+app.set("views", join(__dirname, "views"));
+app.use(logger("dev"));
+app.use(express.static(join(__dirname, "static")));
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -17,4 +19,20 @@ const handleListening = () =>
 
 const server = app.listen(PORT, handleListening);
 
-const io = socketIO(server);
+const io = socketIO.listen(server);
+
+let sockets = [];
+
+io.on("connection", (socket) => {
+  socket.on("newMessage", ({ message }) => {
+    socket.broadcast.emit("messageNotif", {
+      message,
+      nickname: socket.nickname || "Anonymous",
+    });
+  });
+  socket.on("setNickname", ({ nickname }) => {
+    socket.nickname = nickname;
+  });
+});
+
+// setInterval(() => console.log(sockets), 1000);
